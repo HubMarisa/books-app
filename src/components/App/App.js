7d5b71d386ae0895b4a63001f3325fa885ea33f4
“HubMarisa“ <Git2022Hub@protonmail.com>
2023-06-08 17:30:06 +0600

    UseContext + ThemeToggler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,27 +8,36 @@ import BooksList from '../BooksList/BooksList';
 import SearchBar from '../SearchBar/SearchBar';
 import BookDetails from '../BooksList/BookDetails/BookDetails';
 import NotFound from '../NotFound/NotFound';
-
+import { ThemeContext } from '../../context';
+import ThemeToggler from '../ThemeToggler/ThemeToggler';
 
 function App() {
   const [search, setSearch] = useState('');
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }
 
   return (   
-    <Router>
-      <div className="app">
-        <SearchBar setSearch={setSearch} />
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <Router>
+        <div className="app">
+          <SearchBar setSearch={setSearch} />
+          <ThemeToggler />
 
-        <Routes>
-          <Route path='/' element={<BooksList search={search} />}></Route>
-          <Route path='/book/:bookId' element={<BookDetails />}></Route>
+          <Routes>
+            <Route path='/' element={<BooksList search={search} />}></Route>
+            <Route path='/book/:bookId' element={<BookDetails />}></Route>
 
-          <Route path='/about' element={<About />}></Route>
+            <Route path='/about' element={<About />}></Route>
 
-          <Route path='*' element={<Navigate to="/404" />} />
-          <Route path="/404" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path='*' element={<Navigate to="/404" />} />
+            <Route path="/404" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
