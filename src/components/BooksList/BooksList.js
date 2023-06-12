38709c63933ccdcf36347fa270ffcfa579ec927e
“HubMarisa“ <Git2022Hub@protonmail.com>
2023-06-12 12:33:27 +0600

    add useMemo

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useMemo, useState} from "react";
 import { getBooksBySearchTerm } from "../../api/booksApi";
 import { Link } from "react-router-dom";
 import { SearchContext } from "../../context";
@@ -19,22 +19,25 @@ function BooksList () {
             })
             .catch((error) => console.error(error));
         }
-    },[search])
+    },[search]);
+
+    const memoizedBooks = useMemo(() => 
+        books.map((book, index) => (
+            <li key={index}>
+                <Link to={`/book/${book.id}`} title={book.volumeInfo.title}>
+                    {book.volumeInfo.title}
+                </Link>
+            </li>
+        )),
+        [books]
+    );
 
     return (
         <div className="books">
             <div className="container">
                 <h1>Books</h1>
 
-                <ul>
-                    {books.map((book, index) => (
-                        <li key={index}>
-                            <Link to={`/book/${book.id}`} title={book.volumeInfo.title}>
-                                {book.volumeInfo.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <ul>{memoizedBooks}</ul>
             </div>
         </div>
     );
